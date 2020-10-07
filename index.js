fe01b7535f6d2d1a8857d75003d1a28146b8eb9a
Nide17 <nidedrogba@gmail.com>
2020-10-07 15:01:25 +0200

    routes

--- index.js
+++ index.js
@@ -2,52 +2,8 @@
 const express = require('express');
 const app = express();
 
-// Import mentors
-let mentorsData = require('./mentors');
-
-// Route Handler
-app.get("/json", (req, res) => {
-    res.json({ message: "Hello world" });
-});
-
-
-/*
-//  GET /mentors
-*/
-app.get("/mentors", (req, res) => {
-    res.json(mentorsData);
-});
-
-
-// Get one
-app.get("/mentors/:id", (req, res) => {
-    const mentorId = req.params.id;
-    const mentor = mentorsData.find(_mentor => _mentor.id === mentorId);
-
-    if (mentor) {
-        res.json(mentorsData);
-    } else {
-        res.json({ message: `mentor ${mentorId} doesn't exist` })
-    }
-});
-
-
-/*
-// Post an item
-// POST /items
-*/
-app.post("/mentors", (req, res) => {
-
-    const mentor = req.body;
-    console.log('Adding new mentor: ', mentor);
-
-    // add new mentor to array
-    mentorsData.push(mentor)
-
-    // return updated list
-    res.json(mentorsData);
-});
-
+// Mentors
+app.use('/api/mentors', require('./routes/api/mentors'));
 
 //  app.get("/", (req, res) => {
 //     res.sendFile(__dirname + '/UI/mentors.html');
---
+++ routes/api/mentors.js
@@ -0,0 +1,81 @@
+// index.js
+const express = require('express');
+const app = express();
+
+var bodyParser = require('body-parser')
+
+const mentorRouter = express.Router();
+
+mentorRouter.use(bodyParser.json());
+
+// Init middleware
+app.use(express.json({ extended: false }))
+
+// Import mentors
+let mentorsData = require('../../mentors');
+
+
+/*
+GET /mentors
+Get all mentors
+*/
+mentorRouter.get('/', (req, res) => {
+
+    if (!mentorsData) {
+        return res.status(401).json({
+            "status": 401,
+            "error": "Mentors not found!"
+        })
+    }
+    res.json(
+        {
+            "status": 201,
+            "message": "Mentors list",
+            "data": mentorsData
+        }
+    );
+});
+
+
+// Get one
+mentorRouter.get("/:id", (req, res) => {
+    const mentorId = req.params.id;
+    const mentor = mentorsData.find(_mentor => _mentor.id === mentorId);
+
+    if (mentor) {
+        res.status(201).json({
+            "status": 201,
+            "message": "Mentor found",
+            "data": mentor
+        });
+    } else {
+        res.status(401).json({
+            "status": 401,
+            "error": `mentor ${mentorId} doesn't exist`
+        })
+    }
+});
+
+
+/*
+// Post an item
+// POST /items
+*/
+mentorRouter.post('/', (req, res) => {
+
+    const mentor = req.body;
+    console.log('Adding new mentor: ', mentor);
+
+    // add new mentor to array
+    mentorsData.push(mentor)
+    // console.log(req.body);
+
+    // return updated list
+    res.status(201).json({
+        "status": 201,
+        "message": "Mentors list",
+        "data": mentorsData
+    });
+});
+
+module.exports = mentorRouter;
